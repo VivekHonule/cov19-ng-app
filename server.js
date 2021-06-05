2be2496ef79325dc3537855f49eadbceff138d2f
Vivek Honule <honule@esteco.com>
2021-06-05 15:05:38 +0530

    Configuring Heroku build

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const ngApp = express();
+ngApp.use(express.static('./dist/cov19-ng-app'));
+ngApp.get('/*', function (request, response) {
+  response.sendFile(path.join(__dirname, '/dist/cov19-ng-app/index.html'));
+});
+ngApp.listen(process.env.PORT || 8080);
